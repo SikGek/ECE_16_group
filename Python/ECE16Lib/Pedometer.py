a9d62aac50f1c77e9272ed2321d3f0b759a5ceab
wrblynch <64241985+wrblynch@users.noreply.github.com>
2022-05-09 00:05:12 -0700

    Add files via upload
    made filtering changes, include jump count

--- Python/ECE16Lib/Pedometer.py
+++ Python/ECE16Lib/Pedometer.py
@@ -10,21 +10,24 @@ class Pedometer:
   Encapsulated class attributes (with default values)
   """
   __steps = 0        # the current step count
+  __jumps = 0
   __l1 = None        # CircularList containing L1-norm
   __filtered = None  # CircularList containing filtered signal
-  __num_samples = 0  # The length of data maintained
-  __new_samples = 0  # How many new samples exist to process
-  __fs = 0           # Sampling rate in Hz
+  __num_samples = 1500  # The length of data maintained
+  __new_samples = 1500  # How many new samples exist to process
+  __fs = 300           # Sampling rate in Hz
   __b = None         # Low-pass coefficients
   __a = None         # Low-pass coefficients
-  __thresh_low = 10   # Threshold from Tutorial 2
-  __thresh_high = 40 # Threshold from Tutorial 2
-
+  __thresh_walk_low = 10   # Threshold from Tutorial 2
+  __thresh_walk_high = 20 # Threshold from Tutorial 2
+  __thresh_jump_low = 20
+  __thresh_jump_high = 50
   """
   Initialize the class instance
   """
   def __init__(self, num_samples, fs, data=None):
     self.__steps = 0
+    self.__jumps = 0
     self.__num_samples = num_samples
     self.__fs = fs
     self.__l1 = CircularList(data, num_samples)
@@ -53,30 +56,27 @@ class Pedometer:
     # Grab only the new samples into a NumPy array
     x = np.array(self.__l1[ -self.__new_samples: ])
 
-    # Filter the signal (detrend, LP, MA, etc…)
-    # ...
-    # ...
-    # ... 
-    dt = filt.detrend(x)                              # Detrend the Signal
-    b, a = filt.create_filter(3, 1, "lowpass", self.__fs)   # Low-pass Filter Design
-    lp = filt.filter(b, a, dt)                       # Low-pass Filter Signal
+    dt = filt.detrend(x)  # Detrend the Signal
+    b, a = filt.create_filter(3, 1, "lowpass", self.__fs)  # Low-pass Filter Design
+    lp = filt.filter(b, a, dt)  # Low-pass Filter Signal
     grad = filt.gradient(lp)
-    ma = filt.moving_average(grad, 20)                   # Compute Moving Average
-    
-    
+    ma = filt.moving_average(grad, 20)  # Compute Moving Average
 
     # Store the filtered data
     self.__filtered.add(ma.tolist())
 
     # Count the number of peaks in the filtered data
-    count, peaks = filt.count_peaks(ma,self.__thresh_low,self.__thresh_high)
+    step_count, peaks = filt.count_peaks(lp,self.__thresh_walk_low,self.__thresh_walk_high)
+
+    jump_count, peaks = filt.count_peaks(lp, self.__thresh_jump_low, self.__thresh_jump_high)
 
     # Update the step count and reset the new sample count
-    self.__steps += count
+    self.__steps += step_count
+    self.__jumps += jump_count
     self.__new_samples = 0
 
     # Return the step count, peak locations, and filtered data
-    return self.__steps, peaks, np.array(self.__filtered)
+    return self.__steps, self.__jumps, peaks, np.array(self.__filtered)
 
   """
   Clear the data buffers and step count
@@ -84,4 +84,3 @@ class Pedometer:
   def reset(self):
     self.__steps = 0
     self.__l1.clear()
-    self.__filtered = np.zeros(self.__num_samples)
